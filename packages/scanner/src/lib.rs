f812845f97ffa6e03a471f3f9ef25348b4fbac13
Michael Matuzak <mmatuzak@gmail.com>
2019-07-16 14:26:51 -0700

    feat: `import`s!
    This parses some various imports including dynamic imports. There is
    probably some broken stuff, but I think it works overall.

--- packages/scanner/src/lib.rs
+++ packages/scanner/src/lib.rs
@@ -11,7 +11,16 @@ pub fn parse_js<'a>(js: &'a str) -> Vec<&'a str> {
             match the_part {
                 ProgramPart::Decl(some_part) => {
                     pd = match_declaration(some_part, pd)
-                }
+                },
+
+                // Dynamic imports
+                ProgramPart::Stmt(some_part) => {
+                    if let Stmt::Expr(expr) = some_part {
+                        if let Expr::Call(call) = expr {
+                            pd = match_expr(call, pd)
+                        }
+                    }
+                },
                 the_thing => println!("Not a program part: {:?}", the_thing),
             }
         }
@@ -21,9 +30,18 @@ pub fn parse_js<'a>(js: &'a str) -> Vec<&'a str> {
 
 fn match_declaration<'a>(declaration: Decl<'a>, mut pd: Vec<&'a str>) -> Vec<&'a str> {
     match declaration {
-        Decl::Import(import) => println!("{:?}", import),
+        Decl::Import(import) => pd = match_import(import, pd),
         Decl::Variable(_, variable_vec) => pd = match_variable(variable_vec, pd),
         the_thing => println!("log_declaration: {:?}", the_thing),
+    }
+    pd
+}
+
+fn match_import<'a>(import: Box<decl::ModImport<'a>>, mut pd: Vec<&'a str>) -> Vec<&'a str> {
+    let source = import.source;
+    if let expr::Literal::String(string_arg) = source {
+        let quotes: &[_] = &['\'', '\"'];
+        pd.push(string_arg.trim_matches(quotes));
     }
     pd
 }
@@ -43,12 +61,10 @@ fn match_variable<'a>(
     pd
 }
 
-fn match_expr<'a>(
-    expression: resast::ref_tree::expr::CallExpr<'a>,
-    mut pd: Vec<&'a str>,
-) -> Vec<&'a str> {
+fn match_expr<'a>(expression: expr::CallExpr<'a>, mut pd: Vec<&'a str>,) -> Vec<&'a str> {
     if let Expr::Ident(callee) = *expression.callee {
-        if callee == "require" {
+        println!("callee: {:?}", callee);
+        if callee == "require" || callee == "import" {
             if let Some(argument) = expression.arguments.get(0) {
                 println!("{:?}", argument);
                 if let expr::Expr::Literal(literal) = argument {
@@ -74,6 +90,26 @@ mod tests {
     #[test]
     fn top_level_require_double_quotes() {
         let js = "const _ = require(\"lodash\");";
+        assert_eq!(&"lodash", parse_js(js).get(0).unwrap())
+    }
+    #[test]
+    fn top_level_default_import() {
+        let js = "import _ from 'lodash';";
+        assert_eq!(&"lodash", parse_js(js).get(0).unwrap())
+    }
+    #[test]
+    fn top_level_import_alias() {
+        let js = "import * as lodash from \"lodash\";";
+        assert_eq!(&"lodash", parse_js(js).get(0).unwrap())
+    }
+    #[test]
+    fn top_level_import_named() {
+        let js = "import { map } from \"lodash\";";
+        assert_eq!(&"lodash", parse_js(js).get(0).unwrap())
+    }
+    #[test]
+    fn dynamic_import() {
+        let js = "import('lodash');";
         assert_eq!(&"lodash", parse_js(js).get(0).unwrap())
     }
 }
