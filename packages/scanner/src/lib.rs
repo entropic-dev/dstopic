391ece330023b9b80204eb2605423b18866f7caf
Michael Matuzak <mmatuzak@gmail.com>
2019-07-24 15:18:07 -0700

    remove import and parse require statements

--- packages/scanner/src/lib.rs
+++ packages/scanner/src/lib.rs
@@ -12,7 +12,14 @@ pub fn parse_js<'a>(js: &'a str) -> Vec<&'a str> {
                 ProgramPart::Decl(some_part) => {
                     pd = match_declaration(some_part, pd)
                 },
-                the_thing => println!("Not a program part: {:?}", the_thing),
+                ProgramPart::Stmt(some_part) => {
+                    if let Stmt::Expr(expr) = some_part {
+                        if let Expr::Call(call) = expr {
+                            pd = match_expr(call, pd)
+                        }
+                    }
+                },
+                _ => (),
             }
         }
     }
@@ -23,7 +30,7 @@ fn match_declaration<'a>(declaration: Decl<'a>, mut pd: Vec<&'a str>) -> Vec<&'a
     match declaration {
         Decl::Import(import) => pd = match_import(import, pd),
         Decl::Variable(_, variable_vec) => pd = match_variable(variable_vec, pd),
-        the_thing => println!("log_declaration: {:?}", the_thing),
+        _ => (),
     }
     pd
 }
@@ -45,7 +52,7 @@ fn match_variable<'a>(
         if let Some(variable_init) = v.init {
             match variable_init {
                 Expr::Call(call) => pd = match_expr(call, pd),
-                the_thing => println!("log_require: {:?}", the_thing),
+                _ => (),
             }
         }
     }
@@ -54,10 +61,8 @@ fn match_variable<'a>(
 
 fn match_expr<'a>(expression: expr::CallExpr<'a>, mut pd: Vec<&'a str>,) -> Vec<&'a str> {
     if let Expr::Ident(callee) = *expression.callee {
-        println!("callee: {:?}", callee);
-        if callee == "require" || callee == "import" {
+        if callee == "require" {
             if let Some(argument) = expression.arguments.get(0) {
-                println!("{:?}", argument);
                 if let expr::Expr::Literal(literal) = argument {
                     if let expr::Literal::String(string_arg) = literal {
                         let quotes: &[_] = &['\'', '\"'];
@@ -81,6 +86,11 @@ mod tests {
     #[test]
     fn top_level_require_double_quotes() {
         let js = "const _ = require(\"lodash\");";
+        assert_eq!(&"lodash", parse_js(js).get(0).unwrap())
+    }
+    #[test]
+    fn top_level_require_statement() {
+        let js = "require(\"lodash\");";
         assert_eq!(&"lodash", parse_js(js).get(0).unwrap())
     }
     #[test]
